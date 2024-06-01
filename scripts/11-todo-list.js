15730127545803d4bebc61d155d0f01e4db3015e
George Boahene <39881594+olibo1zeemoni@users.noreply.github.com>
2024-06-01 04:58:34 +0000

    generate html with javascript

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,73 @@
+const todoList = [];
+const todoList2 = [];
+const todoList3 = [{
+  name: 'wash clothes', 
+  date: '31-05-2024'
+}];
+
+
+function addTodo(){
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  if (name.length < 3) {
+    return;
+  }
+  todoList.push(name);
+  console.log(todoList);
+  inputElement.value = '';
+};
+
+function addTodo2(){
+  const inputElement = document.querySelector('.js-name-input2');
+  const name = inputElement.value;
+  if (name.length < 3) {
+    return;
+  }
+  todoList2.push(name);
+  inputElement.value = '';
+  let i, text='';
+  for (i=0;i<todoList2.length; i++) {
+    text += todoList2[i] + "<br>"
+  };
+  document.querySelector('.js-show-list').innerHTML = text;
+};
+
+function addTodo3(){
+  const inputNameElement = document.querySelector('.js-todo-input3');
+  const name = inputNameElement.value;
+  const inputDateElement = document.querySelector('.js-todo-date');
+  const date = inputDateElement.value;
+  if (name.length < 3) {
+    return;
+}; 
+todoList3.push({
+  name,
+  date
+});
+inputNameElement.value = '';
+inputDateElement.value = '';
+renderTodoList();
+};
+
+function renderTodoList(){
+  let todoListHtml = '';
+ 
+  let i = 0;
+  for(; i < todoList3.length; i++){
+    const todoObject = todoList3[i];
+    const {name, date} = todoObject;
+    const html = `
+    <p>
+    ${name}  ${date} 
+    <button onclick="
+    todoList3.splice(${i}, 1);
+    renderTodoList();
+    "
+    >Delete</button>
+    </p>
+    `;
+    todoListHtml += html;
+    document.querySelector('.js-todo-list3').innerHTML = todoListHtml;
+  }
+  console.log(todoListHtml);  
+};
